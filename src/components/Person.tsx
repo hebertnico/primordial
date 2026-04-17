4d5f8ce5421593b45f3f45968db1fa623f924915
hebertnico <hebertniconahason@gmail.com>
2026-04-17 22:01:58 +0700

    animate Person

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,5 +1,5 @@
-import { AnimatePresence, motion } from "motion/react";
-import { useState } from "react";
+import { AnimatePresence, hover, motion } from "motion/react";
+import { useEffect, useRef, useState } from "react";
 
 function Person({
   person = "",
@@ -14,7 +14,24 @@ function Person({
   initial = {},
 }) {
   const [isToggled, setIsToggled] = useState(false);
-  classname = classname + " absolute -translate-x-1/2";
+  const [index, setIndex] = useState("z-10");
+  classname = classname + " absolute -translate-x-1/2 cursor-pointer " + index;
+  const ref = useRef(null);
+
+  useEffect(() => {
+    return hover(ref.current, () => {
+      console.log("on hover start");
+      setIsToggled(true);
+      setIndex("z-50");
+
+      return () => {
+        console.log("on hover end");
+        setIsToggled(false);
+        setIndex("z-10");
+      };
+    });
+  }, []);
+
   return (
     <AnimatePresence initial={true}>
       {isVisible ? (
@@ -24,30 +41,56 @@ function Person({
           animate={{ opacity: 1, ...animate }}
           transition={transition}
           onClick={onClick}
-          exit={exit}
-        >
+          exit={exit}>
           <motion.div
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.95 }}
             // onClick={() => setIsToggled(!isToggled)}
-            animate={{ scale: isToggled ? 1.5 : 1 }}
+            // animate={{ scale: isToggled ? 1.5 : 1 }}
             transition={{ type: "spring", stiffness: 300, damping: 20 }}
-            className="relative flex flex-col items-center gap-2"
-          >
-            <div
-              className={`${size} bg-white border-5 border-red-500 p-2 rounded-full cursor-pointer shadow-lg`}
-            >
+            className="flex flex-col items-center gap-1">
+            <motion.div
+              ref={ref}
+              transition={{
+                duration: 3,
+                repeat: Infinity,
+                ease: "easeInOut",
+              }}
+              initial={{ offsetDistance: "0%" }}
+              animate={{ offsetDistance: "100%" }}
+              style={{
+                offsetPath: `path("M 9 11 C 14 7 12 3 8 3 C 4 3 1 3 2 7 C 3 11 5 14 9 11"
+                )`,
+                offsetRotate: "0deg",
+                offsetPosition: "center",
+              }}
+              className={`${size} relative translate-x-[40%] translate-y-1/3 bg-red-500 p-2 rounded-full shadow-lg`}>
               {photo ? (
                 <img
                   src={photo}
                   alt={person}
-                  className="w-full h-full object-cover rounded-full"
+                  className="z-10 w-full h-full object-cover rounded-full"
                 />
               ) : (
                 <></>
               )}
-            </div>
-            <p>{person}</p>
+              <motion.div
+                animate={
+                  isToggled
+                    ? { scale: 1.5, borderWidth: 100 }
+                    : {
+                        scale: 1,
+                        borderWidth: 5,
+                      }
+                }
+                className={`${size} -z-10 absolute top-0 left-0 bg-white border-5 border-red-500 rounded-full`}
+              />
+            </motion.div>
+            <motion.p
+              className="z-20 font-bold"
+              animate={{ color: isToggled ? "black" : "white" }}>
+              {person}
+            </motion.p>
           </motion.div>
         </motion.div>
       ) : null}
