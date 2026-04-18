6de1e61d0afae8421508ce87db356d12f156f6de
hebertnico <hebertniconahason@gmail.com>
2026-04-18 18:40:52 +0700

    modify name card, add tubu and monding

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -3,6 +3,8 @@ import { useEffect, useRef, useState } from "react";
 
 function Person({
   person = "",
+  tubu = "dd Mmm YYYY",
+  monding = "dd Mmm YYYY",
   photo = "",
   size = "size-25",
   classname = "",
@@ -16,6 +18,8 @@ function Person({
   const [isToggled, setIsToggled] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
   const [index, setIndex] = useState("z-10");
+  const longName = person.length > 20;
+  // console.log(longName, person.length, person);
   classname = classname + " absolute -translate-x-1/2 cursor-pointer " + index;
   // const ref = useRef(null);
 
@@ -60,14 +64,15 @@ function Person({
               setIndex(isToggled ? "z-50" : "z-10");
             }}
             // animate={{ scale: isToggled ? 1.5 : 1 }}
-            transition={{ type: "spring", stiffness: 300, damping: 20 }}
-            className={`${size} flex flex-col items-center gap-1`}>
+            // transition={{ type: "spring", stiffness: 300, damping: 20 }}
+            className={`flex flex-col items-center gap-1`}>
             <motion.div
               // ref={ref}
               transition={{
-                duration: 3,
+                duration: 5,
                 repeat: Infinity,
-                ease: "easeInOut",
+                ease: "linear",
+                delay: Math.random() * -10,
               }}
               initial={{ offsetDistance: "0%" }}
               animate={{ offsetDistance: "100%" }}
@@ -77,24 +82,59 @@ function Person({
                 offsetRotate: "0deg",
                 offsetPosition: "center",
               }}
-              className={`${size} relative translate-x-[40%] translate-y-1/3 p-1 rounded-full shadow-lg`}>
-              <img
-                src={photo ? photo : "/images/def_M.jpg"}
-                alt={person}
-                className="z-10 size-full object-cover rounded-full"
-              />
+              className={`${size} relative translate-x-[40%] translate-y-1/3 rounded-full shadow-lg`}>
               <motion.div
                 animate={
-                  isHovered || isToggled ? { width: "200%" } : { width: "100%" }
+                  isHovered || isToggled
+                    ? { scale: 2 }
+                    : { scale: 1, borderWidth: 5 }
                 }
-                className={`${size} -z-10 absolute top-0 left-0 bg-red-500 rounded-full`}
-              />
+                className={`${size} overflow-hidden -z-10 absolute top-0 left-0 bg-red-500 border-red-500 rounded-full`}>
+                <motion.img
+                  animate={
+                    isHovered || isToggled
+                      ? { y: 40, scale: 1.4 / 2 }
+                      : { y: 0, scale: 1 }
+                  }
+                  src={photo ? photo : "/images/def_M.jpg"}
+                  alt={person}
+                  className="size-full object-cover rounded-full"
+                />
+              </motion.div>
+              <motion.p
+                className="absolute font-bold z-20 wrap-break-word top-36 mt-2"
+                animate={
+                  isHovered || isToggled
+                    ? {
+                        x: "-10%",
+                        top: longName ? "-34%" : "-30%",
+                        width: "120%",
+                      }
+                    : { width: "100%" }
+                }>
+                {person}
+              </motion.p>
+              <motion.div
+                className={
+                  monding != ""
+                    ? longName
+                      ? "absolute -translate-x-2 flex flex-col bottom-[72%] w-40"
+                      : "absolute -translate-x-2 flex flex-col bottom-[75%] w-40"
+                    : ""
+                }
+                animate={
+                  isHovered || isToggled ? { opacity: 100 } : { opacity: 0 }
+                }>
+                <p className="text-xs">
+                  Tubu: <span>{tubu}</span>
+                </p>
+                {monding && (
+                  <p className="text-xs">
+                    Monding: <span>{monding}</span>
+                  </p>
+                )}
+              </motion.div>
             </motion.div>
-            <motion.p
-              className="w-full font-bold z-20 break-word"
-              animate={{ color: isToggled || isHovered ? "black" : "white" }}>
-              {person}
-            </motion.p>
           </motion.div>
         </motion.div>
       ) : null}
