4c9c62a656c5aedb5e077ac5b295548d42ecc2cb
hebertnico <hebertniconahason@gmail.com>
2026-06-08 14:51:47 +0700

    put navbar z-index to most top

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { motion, AnimatePresence } from "motion/react";
 
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 z-50 bg-my-cream/80 backdrop-blur-md shadow-md">
+    <nav className="sticky top-0 z-100 bg-my-cream/80 backdrop-blur-md shadow-md">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-10">
           {/* Desktop Nav */}
