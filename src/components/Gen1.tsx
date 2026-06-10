68e25009377b631392a48c1c17adb79aab991b47
hebertnico <hebertniconahason@gmail.com>
2026-06-10 10:45:35 +0700

    hide old attempt

--- src/components/Gen1.tsx
+++ src/components/Gen1.tsx
@@ -1,91 +1,91 @@
-import { useEffect, useState } from "react";
-import Person from "./Person";
-import Gen2 from "./Gen2";
+// import { useEffect, useState } from "react";
+// import Person from "./Person";
+// import Gen2 from "./Gen2";
 
-function Gen1({
-  isVisible = false,
-  g1 = "",
-  onGen2Click = (data: boolean) => {},
-}) {
-  const [members, setMembers] = useState({
-    pasangan: [],
-    anak: [],
-    posP: [],
-    posA: [],
-    fam: [],
-  });
-  const [gen2, setGen2] = useState("none");
-  const [famGen2, setFamGen2] = useState({
-    pasangan: [],
-    anak: [],
-    posP: [],
-    posA: [],
-    fam: [],
-  });
-  const [gen2Visible, setGen2Visible] = useState(false);
+// function Gen1({
+//   isVisible = false,
+//   g1 = "",
+//   onGen2Click = (data: boolean) => {},
+// }) {
+//   const [members, setMembers] = useState({
+//     pasangan: [],
+//     anak: [],
+//     posP: [],
+//     posA: [],
+//     fam: [],
+//   });
+//   const [gen2, setGen2] = useState("none");
+//   const [famGen2, setFamGen2] = useState({
+//     pasangan: [],
+//     anak: [],
+//     posP: [],
+//     posA: [],
+//     fam: [],
+//   });
+//   const [gen2Visible, setGen2Visible] = useState(false);
 
-  const handleGen2Click = (name: string, fam: any) => {
-    if (gen2 === name) {
-      setGen2("none");
-      setFamGen2({ pasangan: [], anak: [], posP: [], posA: [], fam: [] });
-      onGen2Click(false);
-      setGen2Visible(false);
-      history.replaceState({ page: "rg1", person: g1 }, "", `/rg1/${g1}`);
-    } else {
-      onGen2Click(true);
-      setGen2Visible(true);
-      setGen2(name);
-      setFamGen2(fam);
-      history.pushState(
-        { page: "rg1", person: g1, gen2: name },
-        "",
-        `/rg1/${g1}/${name}`,
-      );
-    }
-  };
+//   const handleGen2Click = (name: string, fam: any) => {
+//     if (gen2 === name) {
+//       setGen2("none");
+//       setFamGen2({ pasangan: [], anak: [], posP: [], posA: [], fam: [] });
+//       onGen2Click(false);
+//       setGen2Visible(false);
+//       history.replaceState({ page: "rg1", person: g1 }, "", `/rg1/${g1}`);
+//     } else {
+//       onGen2Click(true);
+//       setGen2Visible(true);
+//       setGen2(name);
+//       setFamGen2(fam);
+//       history.pushState(
+//         { page: "rg1", person: g1, gen2: name },
+//         "",
+//         `/rg1/${g1}/${name}`,
+//       );
+//     }
+//   };
 
-  useEffect(() => {
-    import(`../data/members/${g1}.json`)
-      .then((module) => setMembers(module.default))
-      .catch((error) => {
-        // console.error("Error loading members json data:", error);
-        setMembers({ pasangan: [], anak: [], posP: [], posA: [], fam: [] });
-      });
-  }, [g1]);
+//   useEffect(() => {
+//     import(`../data/members/${g1}.json`)
+//       .then((module) => setMembers(module.default))
+//       .catch((error) => {
+//         // console.error("Error loading members json data:", error);
+//         setMembers({ pasangan: [], anak: [], posP: [], posA: [], fam: [] });
+//       });
+//   }, [g1]);
 
-  if (!members) {
-    return null; // or a loading indicator
-  }
+//   if (!members) {
+//     return null; // or a loading indicator
+//   }
 
-  return (
-    <>
-      {/* pasangan */}
-      <Person
-        person={members.pasangan[0]}
-        isVisible={isVisible && !gen2Visible}
-        // animate={rg1act ? { top: 200, left: 1000 } : {}}
-      ></Person>
-      {members.pasangan.length > 1 && (
-        <Person
-          person={members.pasangan[1]}
-          isVisible={isVisible && !gen2Visible}
-        ></Person>
-      )}
+//   return (
+//     <>
+//       {/* pasangan */}
+//       <Person
+//         person={members.pasangan[0]}
+//         isVisible={isVisible && !gen2Visible}
+//         // animate={rg1act ? { top: 200, left: 1000 } : {}}
+//       ></Person>
+//       {members.pasangan.length > 1 && (
+//         <Person
+//           person={members.pasangan[1]}
+//           isVisible={isVisible && !gen2Visible}
+//         ></Person>
+//       )}
 
-      {/* anak */}
-      {members.anak &&
-        members.anak.map((person) => (
-          <Person
-            key={person}
-            person={person}
-            isVisible={isVisible && (!gen2Visible || gen2 === person)}
+//       {/* anak */}
+//       {members.anak &&
+//         members.anak.map((person) => (
+//           <Person
+//             key={person}
+//             person={person}
+//             isVisible={isVisible && (!gen2Visible || gen2 === person)}
 
-            // animate={rg1act ? { top: 200, left: 1000 } : {}}
-          ></Person>
-        ))}
-      <Gen2 g2={gen2} isVisible={gen2Visible} members={famGen2} />
-    </>
-  );
-}
+//             // animate={rg1act ? { top: 200, left: 1000 } : {}}
+//           ></Person>
+//         ))}
+//       <Gen2 g2={gen2} isVisible={gen2Visible} members={famGen2} />
+//     </>
+//   );
+// }
 
-export default Gen1;
+// export default Gen1;
--- src/components/Gen2.tsx
+++ src/components/Gen2.tsx
@@ -1,56 +1,56 @@
-// import { useEffect, useState } from "react";
-import Person from "./Person";
+// // import { useEffect, useState } from "react";
+// import Person from "./Person";
 
-interface Members {
-  pasangan: any[];
-  anak: any[];
-  posP: string[];
-  posA: string[];
-}
+// interface Members {
+//   pasangan: any[];
+//   anak: any[];
+//   posP: string[];
+//   posA: string[];
+// }
 
-function Gen2({ isVisible = false, g2 = "", members = {} as Members }) {
-  //   useEffect(() => {
-  //     import(`../data/members/${g2}.json`)
-  //       .then((module) => setMembers(module.default))
-  //       .catch((error) => {
-  //         // console.error("Error loading members json data:", error);
-  //         setMembers({ pasangan: [], anak: [], posP: [], posA: [] });
-  //       });
-  //   }, [g2]);
+// function Gen2({ isVisible = false, g2 = "", members = {} as Members }) {
+//   //   useEffect(() => {
+//   //     import(`../data/members/${g2}.json`)
+//   //       .then((module) => setMembers(module.default))
+//   //       .catch((error) => {
+//   //         // console.error("Error loading members json data:", error);
+//   //         setMembers({ pasangan: [], anak: [], posP: [], posA: [] });
+//   //       });
+//   //   }, [g2]);
 
-  if (!members) {
-    return null; // or a loading indicator
-  }
+//   if (!members) {
+//     return null; // or a loading indicator
+//   }
 
-  return (
-    <>
-      {/* pasangan */}
-      <Person
-        person={members.pasangan[0]}
-        isVisible={isVisible}
-        // transition={{ duration: 1, ease: "easeInOut" }}
-        // animate={rg1act ? { top: 200, left: 1000 } : {}}
-      ></Person>
-      {members.pasangan.length > 1 && (
-        <Person
-          person={members.pasangan[1]}
-          isVisible={isVisible}
-          // transition={{ duration: 1, ease: "easeInOut" }}
-        ></Person>
-      )}
+//   return (
+//     <>
+//       {/* pasangan */}
+//       <Person
+//         person={members.pasangan[0]}
+//         isVisible={isVisible}
+//         // transition={{ duration: 1, ease: "easeInOut" }}
+//         // animate={rg1act ? { top: 200, left: 1000 } : {}}
+//       ></Person>
+//       {members.pasangan.length > 1 && (
+//         <Person
+//           person={members.pasangan[1]}
+//           isVisible={isVisible}
+//           // transition={{ duration: 1, ease: "easeInOut" }}
+//         ></Person>
+//       )}
 
-      {/* anak */}
-      {members.anak &&
-        members.anak.map((person) => (
-          <Person
-            key={person}
-            person={person}
-            isVisible={isVisible}
-            // animate={rg1act ? { top: 200, left: 1000 } : {}}
-          ></Person>
-        ))}
-    </>
-  );
-}
+//       {/* anak */}
+//       {members.anak &&
+//         members.anak.map((person) => (
+//           <Person
+//             key={person}
+//             person={person}
+//             isVisible={isVisible}
+//             // animate={rg1act ? { top: 200, left: 1000 } : {}}
+//           ></Person>
+//         ))}
+//     </>
+//   );
+// }
 
-export default Gen2;
+// export default Gen2;
