b9878a7aadd6d94659596afc183a621b778af1dd
hebertnico <hebertniconahason@gmail.com>
2026-06-17 17:01:32 +0700

    modify page

--- src/pages/SpecialTree.tsx
+++ src/pages/SpecialTree.tsx
@@ -5,12 +5,16 @@ import position from "../data/position.json" with { type: "json" };
 import { AnimatePresence, motion, scale } from "motion/react";
 import { useNodeStore } from "../store/nodeStore";
 import { getChildren, getSpouses } from "../utils/treeHelpers";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { CircleArrowLeft } from "lucide-react";
 
 function SpecialTree() {
   const [loading, setLoading] = useState(true);
   const [activeId, setActiveId] = useState<string>("");
   const [expandId, setExpandId] = useState<string>("");
+  const [secondPlace, setSecondPlace] = useState(true);
+
+  const navigate = useNavigate();
 
   let { head = "" } = useParams();
   const nodes = useNodeStore((s) => s.nodes);
@@ -28,26 +32,39 @@ function SpecialTree() {
     if (fam1.length == 2 && fam2.length == 7) {
       setPosA(position.children[10]?.position);
       setPosB(position.children[11]?.position);
+      setSecondPlace(true);
       setLoading(false);
     }
     if (fam1.length == 7 && fam2.length == 2) {
       setPosA(position.children[11]?.position);
       setPosB(position.children[10]?.position);
+      setSecondPlace(false);
       setLoading(false);
     }
   }, []);
 
   return (
     <div className="min-h-screen relative m-auto overflow-x-hidden overflow-y-hidden">
-      {/* <AnimatePresence mode="popLayout"> */}
-      {/* <div className="absolute flex items-center justify-center left-50 bottom-0 bg-yellow-300 w-1 h-100" /> */}
-
+      <motion.div //back button
+        className="absolute flex items-center justify-center left-5 top-5 bg-my-cream size-15 cursor-pointer rounded-full shadow-2xl/80 z-50"
+        onClick={() => {
+          setActiveId("");
+          navigate(-1);
+        }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <CircleArrowLeft size={45} color="var(--color-my-black)" />
+      </motion.div>
       <AnimatePresence mode="sync">
         {famHead && ( //head
           <motion.div
             key={famHead.id}
-            className="absolute top-[50vh] left-[50vw] w-47 h-32 sm:top-[46vh] sm:w-40 sm:h-40 -translate-1/2"
-            style={{ zIndex: famHead.id === activeId ? 40 : 20 }}
+            onLayoutAnimationComplete={() => setExpandId(head)}
+            className="absolute left-[50vw] w-47 h-32 sm:top-[46vh] sm:w-40 sm:h-40 -translate-1/2"
+            style={{
+              zIndex: famHead.id === activeId ? 40 : 20,
+              top: secondPlace ? "33vh" : "61vh",
+            }}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.4 }}
@@ -61,6 +78,7 @@ function SpecialTree() {
           >
             <Person
               id={famHead.id}
+              expanded={expandId}
               person={famHead.name}
               sex={famHead.sex}
               photo={famHead.image ?? ""}
@@ -86,8 +104,11 @@ function SpecialTree() {
           <div>
             <motion.div
               key={spouse[0]?.id}
-              className="absolute top-[36vh] left-[50vw] sm:top-[46vh] sm:left-[27vw] w-40 h-38 sm:w-38 sm:h-38 -translate-1/2"
-              style={{ zIndex: spouse[0].id === activeId ? 40 : 10 }}
+              className="absolute left-[50vw] sm:top-[46vh] sm:left-[27vw] size-38 -translate-1/2"
+              style={{
+                zIndex: spouse[0].id === activeId ? 40 : 10,
+                top: secondPlace ? "20vh" : "48vh",
+              }}
               initial={{ x: 0, y: 0, opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 0.4 }}
@@ -123,8 +144,11 @@ function SpecialTree() {
             </motion.div>
             <motion.div
               key={spouse[1]?.id}
-              className="absolute bottom-[37vh] left-[50vw] w-40 h-38 sm:bottom-[54vh] sm:left-[73vw] sm:w-38 sm:h-38 -translate-x-1/2 translate-y-1/2"
-              style={{ zIndex: spouse[1].id === activeId ? 40 : 10 }}
+              className="absolute left-[50vw] size-38 sm:left-[73vw] -translate-1/2"
+              style={{
+                zIndex: spouse[1].id === activeId ? 40 : 10,
+                top: secondPlace ? "46vh" : "74vh",
+              }}
               initial={{ x: 0, y: 0, opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 0.4 }}
@@ -160,102 +184,118 @@ function SpecialTree() {
             </motion.div>
           </div>
         )}
-        {!loading && //fam1
-          fam1?.map((person: any) => (
-            <motion.div
-              layoutId={person.id}
-              onLayoutAnimationComplete={() => setExpandId("RSRP")}
-              // ref={(el) => {
-              //   zRef.current[index] = el;
-              // }}
-              key={person.id}
-              className={`${posA[person.sibOrder - 1]} absolute`}
-              style={{ zIndex: person.id === activeId ? 40 : 10 }}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.4 }}
-              exit={{ opacity: 0 }}
-              whileHover={{ zIndex: 40 }}
-              onClick={() =>
-                person.id === activeId
-                  ? setActiveId("")
-                  : setActiveId(person.id)
-              }
-            >
-              <Person
-                id={person.id}
-                expanded={expandId}
-                person={person.name}
-                childnum={person.sibOrder}
-                sex={person.sex}
-                photo={person.image}
-                tubu={
-                  person.tubu
-                    ? new Date(person.tubu).toLocaleDateString("id-ID", {
-                        dateStyle: "medium",
-                      })
-                    : ""
+        <div
+          className="absolute"
+          style={{
+            top: secondPlace ? "8vh" : "8vh",
+          }}
+          key="fam1group"
+        >
+          {!loading && //fam1
+            fam1?.map((person: any) => (
+              <motion.div
+                layoutId={person.id}
+                onLayoutAnimationComplete={() => setExpandId(head)}
+                // ref={(el) => {
+                //   zRef.current[index] = el;
+                // }}
+                key={person.id}
+                className={`${posA[person.sibOrder - 1]} absolute`}
+                style={{ zIndex: person.id === activeId ? 40 : 10 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.4 }}
+                exit={{ opacity: 0 }}
+                whileHover={{ zIndex: 40 }}
+                onClick={() =>
+                  person.id === activeId
+                    ? setActiveId("")
+                    : setActiveId(person.id)
                 }
-                monding={
-                  person.monding
-                    ? new Date(person.monding).toLocaleDateString("id-ID", {
-                        dateStyle: "medium",
-                      })
-                    : ""
+              >
+                <Person
+                  id={person.id}
+                  expanded={expandId}
+                  person={person.name}
+                  childnum={person.sibOrder}
+                  sex={person.sex}
+                  photo={person.image}
+                  tubu={
+                    person.tubu
+                      ? new Date(person.tubu).toLocaleDateString("id-ID", {
+                          dateStyle: "medium",
+                        })
+                      : ""
+                  }
+                  monding={
+                    person.monding
+                      ? new Date(person.monding).toLocaleDateString("id-ID", {
+                          dateStyle: "medium",
+                        })
+                      : ""
+                  }
+                  hasFam={person.spouse?.length}
+                  isActive={person.id === activeId ? true : false}
+                />
+              </motion.div>
+            ))}
+        </div>
+        <div
+          className="absolute"
+          style={{
+            top: secondPlace ? "62vh" : "85vh",
+          }}
+          key="fam2group"
+        >
+          {!loading && //fam2
+            fam2?.map((person: any) => (
+              <motion.div
+                layoutId={person.id}
+                onLayoutAnimationComplete={() => setExpandId(head)}
+                // ref={(el) => {
+                //   zRef.current[index] = el;
+                // }}
+                key={person.id}
+                className={`${posB[person.sibOrder - 1]} absolute`}
+                style={{ zIndex: person.id === activeId ? 40 : 10 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.4 }}
+                exit={{ opacity: 0 }}
+                whileHover={{ zIndex: 40 }}
+                onClick={() =>
+                  person.id === activeId
+                    ? setActiveId("")
+                    : setActiveId(person.id)
                 }
-                hasFam={person.spouse?.length}
-                isActive={person.id === activeId ? true : false}
-              />
-            </motion.div>
-          ))}
-        {!loading && //fam2
-          fam2?.map((person: any) => (
-            <motion.div
-              layoutId={person.id}
-              onLayoutAnimationComplete={() => setExpandId("RSRP")}
-              // ref={(el) => {
-              //   zRef.current[index] = el;
-              // }}
-              key={person.id}
-              className={`${posB[person.sibOrder - 1]} absolute`}
-              style={{ zIndex: person.id === activeId ? 40 : 10 }}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.4 }}
-              exit={{ opacity: 0 }}
-              whileHover={{ zIndex: 40 }}
-              onClick={() =>
-                person.id === activeId
-                  ? setActiveId("")
-                  : setActiveId(person.id)
-              }
-            >
-              <Person
-                id={person.id}
-                expanded={expandId}
-                person={person.name}
-                childnum={person.sibOrder}
-                sex={person.sex}
-                photo={person.image}
-                tubu={
-                  person.tubu
-                    ? new Date(person.tubu).toLocaleDateString("id-ID", {
-                        dateStyle: "medium",
-                      })
-                    : ""
-                }
-                monding={
-                  person.monding
-                    ? new Date(person.monding).toLocaleDateString("id-ID", {
-                        dateStyle: "medium",
-                      })
-                    : ""
-                }
-                hasFam={person.spouse?.length}
-                isActive={person.id === activeId ? true : false}
-              />
-            </motion.div>
-          ))}
+              >
+                <Person
+                  id={person.id}
+                  expanded={expandId}
+                  person={person.name}
+                  childnum={person.sibOrder}
+                  sex={person.sex}
+                  photo={person.image}
+                  tubu={
+                    person.tubu
+                      ? new Date(person.tubu).toLocaleDateString("id-ID", {
+                          dateStyle: "medium",
+                        })
+                      : ""
+                  }
+                  monding={
+                    person.monding
+                      ? new Date(person.monding).toLocaleDateString("id-ID", {
+                          dateStyle: "medium",
+                        })
+                      : ""
+                  }
+                  hasFam={person.spouse?.length}
+                  isActive={person.id === activeId ? true : false}
+                />
+              </motion.div>
+            ))}
+        </div>
       </AnimatePresence>
 
       <AnimatePresence>
